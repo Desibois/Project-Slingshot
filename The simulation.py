a0f7df61715cff4d18d6f476c117aadeb97e8366
Desibois <145998810+Desibois@users.noreply.github.com>
2024-12-03 14:48:35 +0000

    Elastic changes

--- The simulation.py
+++ The simulation.py
@@ -8,49 +8,26 @@ mass2 = float(input('Mass 2/kg: '))
 height2 = float(input('Length of mass 2/m: '))
 h = float(input('Height/m: '))
 t = float(input('Collision time between both masses'))
-
 h += height2
 
+#######################################################################################################################################
 #v^2 = u^2 + 2as --> velocity of both balls when they hit the ground
 velocity = math.sqrt(2*g*h) 
-
 #######################################################################################################################################
-#Ball 2 hits the ground at the v and there is a reaction force from the ground which makes Ball 2 go up at v*e2
-#Ball 2 collides with Ball 1 and momentum is transferred between the 2 balls
-#Newton's 2nd law - F = Δp/Δt --> Δp =F*Δt 
-
-
-
-
-#v1=(1−e)v2
-#This is the formula for the collision between the 2 balls and v1 is the final velocity going up
-#######################################################################################################################################
-
-
-#InitialMomentum = mass1 * velocity
-#rForce = (mass1 + mass2) * g
-#AddedMomentum = rForce*t
-#FinalMomentum = InitialMomentum + AddedMomentum
-
+#Velocity a mass 2 after bouncing off the ground
 v2 = velocity*e2
-v1 = (1-e1)*v2
+
+#Collision between mass1 and mass2
+#Needs work
+v1 = 0
 
 
 
 #######################################################################################################################################
-#We have the momentum of Ball1 going up
-#It's initial velocity can now be calculated
-#From that, displacement can also be calculated using SUVAT
-
-
-
-#We now have the final velocity of the ball going up
 #SUVAT will be used to get displacement
 #######################################################################################################################################
 
-
 u = 0
-#v = FinalMomentum / mass1
 v = v1
 a = -9.81
 
