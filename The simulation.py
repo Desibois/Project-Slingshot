12e4165cd3b3ac4a4ff788fda610c8c7654aac81
Desibois <145998810+Desibois@users.noreply.github.com>
2024-12-03 10:22:35 +0000

    Added coefficients of balls

--- The simulation.py
+++ The simulation.py
@@ -1,6 +1,8 @@
 import math
 
 g = 9.81
+e1 = 0.772 # coefficient of restitution for a tennis ball
+e2 = 0.85  # coefficient of restitution for a basketball
 mass1 = float(input('Mass 1/kg: '))
 mass2 = float(input('Mass 2/kg: '))
 height2 = float(input('Length of mass 2/m: '))
@@ -13,7 +15,7 @@ h += height2
 velocity = math.sqrt(2*g*h) 
 
 #######################################################################################################################################
-#Ball 2 hits the ground at the v and there is a reaction force from the ground which makes Ball 2 go up at v
+#Ball 2 hits the ground at the v and there is a reaction force from the ground which makes Ball 2 go up at v*e2
 #Ball 2 collides with Ball 1 and momentum is transferred between the 2 balls
 #Newton's 2nd law - F = Δp/Δt --> Δp =F*Δt 
 #######################################################################################################################################
