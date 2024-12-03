87ffcf2cf066bfe862c5822bff83ed4a3856db76
Desibois <145998810+Desibois@users.noreply.github.com>
2024-12-03 10:36:34 +0000

    CHanged the program to simulate an elastic collision instead of an inelastic collision

--- The simulation.py
+++ The simulation.py
@@ -18,23 +18,40 @@ velocity = math.sqrt(2*g*h)
 #Ball 2 hits the ground at the v and there is a reaction force from the ground which makes Ball 2 go up at v*e2
 #Ball 2 collides with Ball 1 and momentum is transferred between the 2 balls
 #Newton's 2nd law - F = Δp/Δt --> Δp =F*Δt 
+
+
+
+
+#v1=(1−e)v2
+#This is the formula for the collision between the 2 balls and v1 is the final velocity going up
 #######################################################################################################################################
 
-InitialMomentum = mass1 * velocity
 
-rForce = (mass1 + mass2) * g
-AddedMomentum = rForce*t
-FinalMomentum = InitialMomentum + AddedMomentum
+#InitialMomentum = mass1 * velocity
+#rForce = (mass1 + mass2) * g
+#AddedMomentum = rForce*t
+#FinalMomentum = InitialMomentum + AddedMomentum
+
+v2 = velocity*e2
+v1 = (1-e1)*v2
+
+
 
 #######################################################################################################################################
 #We have the momentum of Ball1 going up
 #It's initial velocity can now be calculated
 #From that, displacement can also be calculated using SUVAT
+
+
+
+#We now have the final velocity of the ball going up
+#SUVAT will be used to get displacement
 #######################################################################################################################################
 
 
 u = 0
-v = FinalMomentum / mass1
+#v = FinalMomentum / mass1
+v = v1
 a = -9.81
 
 s = (v^2 - u^2) /2*a
