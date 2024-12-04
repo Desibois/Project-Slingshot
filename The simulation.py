5d5db69181320c6ca15c6fed0924689be02ac897
Desibois <145998810+Desibois@users.noreply.github.com>
2024-12-04 16:40:00 +0000

    Fixed the final velocity and tidied things up

--- The simulation.py
+++ The simulation.py
@@ -1,8 +1,18 @@
 import math
 
-g = 9.81
+#######################################################################################################################################
+                                             # CONSTANTS
+#######################################################################################################################################
+
+g = 9.81 
 e1 = 0.772 # coefficient of restitution for a tennis ball
 e2 = 0.85  # coefficient of restitution for a basketball
+e = math.sqrt(e1 * e2) # coefficient of restitution between tennis ball and basketball
+
+#######################################################################################################################################
+                                             # INPUTS
+#######################################################################################################################################
+
 mass1 = float(input('Mass 1/kg: '))
 mass2 = float(input('Mass 2/kg: '))
 height2 = float(input('Length of mass 2/m: '))
@@ -11,25 +21,32 @@ t = float(input('Collision time between both masses'))
 h += height2
 
 #######################################################################################################################################
-#v^2 = u^2 + 2as --> velocity of both balls when they hit the ground
-velocity = math.sqrt(2*g*h) 
+                                             # BALLS HIT THE GROUND
 #######################################################################################################################################
+
+#Velocity of balls before they hit the ground
+velocity = math.sqrt(2*g*h) 
+v1 = velocity
+
 #Velocity a mass 2 after bouncing off the ground
 v2 = velocity*e2
 
-#Collision between mass1 and mass2
-#Needs work
-v1 = 0
-
-
-
 #######################################################################################################################################
-#SUVAT will be used to get displacement
+                                             # COLLISION BETWEEN BALLS
 #######################################################################################################################################
 
+#Equation derived from combining Conservation of Momentum and Coefficient of Restitution
+v = ((mass1 - mass2)*v1 + (1+e)*mass2*v2)/(mass1+mass2)
+
+#######################################################################################################################################
+                                             #SUVAT 
+#######################################################################################################################################
+
+#s = ?
 u = 0
-v = v1
+v 
 a = -9.81
+#t - Unnecessary
 
 s = (v^2 - u^2) /2*a
 
